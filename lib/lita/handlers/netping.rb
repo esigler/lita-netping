42f5068a4180960d9586e533f063a35cd3c6ce5c
Eric Sigler <me@esigler.com>
2014-12-09 11:53:53 -0800

    And here's a basic working version.

--- lib/lita/handlers/netping.rb
+++ lib/lita/handlers/netping.rb
@@ -1,6 +1,34 @@
 module Lita
   module Handlers
     class Netping < Handler
+      route(
+        /^ping\s(?<target>.+)/,
+        :ping,
+        command: true,
+        help: {
+          t('help.ping.syntax') => t('help.ping.desc')
+        }
+      )
+
+      def ping(response)
+        target = response.match_data['target']
+
+        response.reply(format(target, ping_target(target)))
+      end
+
+      private
+
+      def format(host, result)
+        if result
+          t('ping.success', host: host)
+        else
+          t('ping.fail', host: host)
+        end
+      end
+
+      def ping_target(host)
+        Net::Ping::External.new(host).ping
+      end
     end
 
     Lita.register_handler(Netping)
--- spec/lita/handlers/netping_spec.rb
+++ spec/lita/handlers/netping_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe Lita::Handlers::Netping, lita_handler: true do
+  it do
+    is_expected.to route_command('ping example.com').to(:ping)
+  end
+
+  describe '#ping' do
+    it 'returns a success if the target is pingable' do
+      send_command('ping example.com')
+      expect(replies.last).to eq('example.com responded to ping')
+    end
+
+    it 'returns a failure if the target is not pingable' do
+      send_command('ping unknownhost.local')
+      expect(replies.last).to eq('unknownhost.local did not respond to ping')
+    end
+  end
 end
